b5965bd60c22a7b3acc5f84b0154fe23c003fe2d
Roberto Guerra <uris77@gmail.com>
2021-06-19 14:55:15 -0600

    adds log when updating person

--- handlers/person_created.go
+++ handlers/person_created.go
@@ -11,8 +11,9 @@ import (
 
 // PersonCreatedResult is the output of PersonCreated
 type PersonCreatedResult struct {
-	Age   int
-	Event models.FirestorePersonEvent
+	Age    int
+	Event  models.FirestorePersonEvent
+	Person models.PersonalInfo
 }
 
 // PersonCreated is a handler that gets triggered when a new person is created.
@@ -24,11 +25,13 @@ func PersonCreated(ctx context.Context, event models.FirestorePersonEvent, perso
 		return PersonCreatedResult{}, fmt.Errorf("could not parse dob: %w", err)
 	}
 
-	if err := personStore.CreatePerson(ctx, event.Value.Fields.ToPerson()); err != nil {
+	person := event.Value.Fields.ToPerson()
+	if err := personStore.CreatePerson(ctx, person); err != nil {
 		return PersonCreatedResult{}, fmt.Errorf("PersonCreated() failed: %w", err)
 	}
 	return PersonCreatedResult{
-		Age:   age.AgeAt(dobDate, event.Value.Fields.Created.TimestampValue),
-		Event: event,
+		Age:    age.AgeAt(dobDate, event.Value.Fields.Created.TimestampValue),
+		Event:  event,
+		Person: person,
 	}, nil
 }
